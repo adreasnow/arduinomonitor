699fae2d0ae249458b473b202f72febfa316c800
Adrea Snow <adrea.snow@gmail.com>
2020-07-16 21:05:19 +1000

    got the code to run on the target machine (no longer just a proof of concept.
    added functionality to convert the ascii strings into bytecode to be able to better utilise the HD44780 protocol (didn't bother with the Japanese, maths or greek characters though

--- arduinorun.py
+++ arduinorun.py
@@ -1,14 +1,12 @@
-import subprocess
 from serial import Serial
 import serial
 import time
 from functions import *
-import binascii
 
 functime = 5
-
-arduino = Serial('/dev/tty.usbmodem1414201', 115200, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
+arduino = Serial('/dev/ttyACM0', 115200, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
 time.sleep(2)
+
 
 # Master loop. loops thorugh all of the sub functions of the display
 while True:
@@ -21,7 +19,7 @@ while True:
 		usage = int(round(16 * (float(runbash("ps -e -o %cpu | awk '{s+=$1} END {print s}'"))/procmult), 0))
 		usagebar = ''
 		for i in range(usage):
-			usagebar = usagebar + '#'
+			usagebar = usagebar + '█'
 
 		printtoarduino(arduino, "CPU utilisation:", usagebar)
 		time.sleep(0.5)
@@ -29,5 +27,9 @@ while True:
 	# simple cpu temp meter
 	endtime = time.time() + functime
 	while endtime > time.time():
-		cputemp = runbash('sudo powermetrics --samplers smc -i1 -n1 | grep "CPU die" | cut -d " " -f 4')
+		cputemp = runbash('sensors k10temp-pci-00c3 | grep "Tctl:" | tr -s " " | cut -d" " -f 2')
 		printtoarduino(arduino, "CPU Temperature:", cputemp)
+		time.sleep(1)
+
+	printtoarduino(arduino, "CPU Governor:", runbash("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"))
+	time.sleep(3)
