37a679b3d1905be82ee9911bcc487d6f566a433b
Adrea Snow <adrea.snow@gmail.com>
2020-08-30 13:49:18 +1000

    commented out the HWmon stuff from Rosalind. I'm enjoying the monitor as a time-based thing

--- arduinorun.py
+++ arduinorun.py
@@ -30,50 +30,50 @@ while True:
 		time.sleep(3)
 
 ###################################### For rosalind #######################################
-		if hostname == "Rosalind":
-			# cpu/gpu power meter
-			endtime = time.time() + functime
-			while endtime > time.time():
-				cpupow = cpupower()
-				gpupow = runbash("sensors | grep 'power1' | tr -s ' ' | cut -d' ' -f2") + "W"
-				printtoarduino(arduino, "CPU  -POWR-  GPU", cpupow + padding(cpupow + gpupow) + gpupow)
-				time.sleep(refreshint)
+		# if hostname == "Rosalind":
+		# 	# cpu/gpu power meter
+		# 	endtime = time.time() + functime
+		# 	while endtime > time.time():
+		# 		cpupow = cpupower()
+		# 		gpupow = runbash("sensors | grep 'power1' | tr -s ' ' | cut -d' ' -f2") + "W"
+		# 		printtoarduino(arduino, "CPU  -POWR-  GPU", cpupow + padding(cpupow + gpupow) + gpupow)
+		# 		time.sleep(refreshint)
 
-			# cpu frequencies
-			endtime = time.time() + functime
-			while endtime > time.time():
-				cpulow, cpuhigh = cpufreq()
-				printtoarduino(arduino, "LOW  -FREQ- HIGH", cpulow + padding(cpuhigh + cpulow) + cpuhigh)
-				time.sleep(refreshint)
+		# 	# cpu frequencies
+		# 	endtime = time.time() + functime
+		# 	while endtime > time.time():
+		# 		cpulow, cpuhigh = cpufreq()
+		# 		printtoarduino(arduino, "LOW  -FREQ- HIGH", cpulow + padding(cpuhigh + cpulow) + cpuhigh)
+		# 		time.sleep(refreshint)
 
-			# cpu/gpu temp meter
-			endtime = time.time() + functime
-			while endtime > time.time():
-				cputemp = runbash('sensors k10temp-pci-00c3 | grep "Tctl:" | tr -s " " | cut -d" " -f 2')
-				gputemp = runbash("sensors | grep 'edge:' | tr -s ' ' | cut -d' ' -f 2")
-				printtoarduino(arduino, "CPU  -TEMP-  GPU", cputemp + "  " + gputemp)
-				time.sleep(refreshint)
+		# 	# cpu/gpu temp meter
+		# 	endtime = time.time() + functime
+		# 	while endtime > time.time():
+		# 		cputemp = runbash('sensors k10temp-pci-00c3 | grep "Tctl:" | tr -s " " | cut -d" " -f 2')
+		# 		gputemp = runbash("sensors | grep 'edge:' | tr -s ' ' | cut -d' ' -f 2")
+		# 		printtoarduino(arduino, "CPU  -TEMP-  GPU", cputemp + "  " + gputemp)
+		# 		time.sleep(refreshint)
 
-			# cpu/gpu usage meter, 
-			endtime = time.time() + functime
-			procmult = int(runbash('nproc'))*100
-			while endtime > time.time():
-				cpuusage = float(runbash("ps -e -o %cpu | awk '{s+=$1} END {print s}'"))/procmult
-				gpuusage = float(runbash("/opt/radeontop/radeontop -d- -l1 | grep -o 'gpu [0-9]\{1,3\}' | cut -c 5-7"))/100
-				usagebarcpu = usagebar(cpuusage, 12)
-				usagebargpu = usagebar(gpuusage, 12)
+		# 	# cpu/gpu usage meter, 
+		# 	endtime = time.time() + functime
+		# 	procmult = int(runbash('nproc'))*100
+		# 	while endtime > time.time():
+		# 		cpuusage = float(runbash("ps -e -o %cpu | awk '{s+=$1} END {print s}'"))/procmult
+		# 		gpuusage = float(runbash("/opt/radeontop/radeontop -d- -l1 | grep -o 'gpu [0-9]\{1,3\}' | cut -c 5-7"))/100
+		# 		usagebarcpu = usagebar(cpuusage, 12)
+		# 		usagebargpu = usagebar(gpuusage, 12)
 
-				printtoarduino(arduino, "CPU:" + usagebarcpu, "GPU:" + usagebargpu, )
-				time.sleep(refreshint)
+		# 		printtoarduino(arduino, "CPU:" + usagebarcpu, "GPU:" + usagebargpu, )
+		# 		time.sleep(refreshint)
 
-			# mem usage meter
-			endtime = time.time() + functime
-			while endtime > time.time():
-				total = float(runbash('cat /proc/meminfo | grep "MemTotal:" | tr -s " " | cut -d" " -f2'))
-				avail = float(runbash('cat /proc/meminfo | grep "MemAvailable:" | tr -s " " | cut -d" " -f2'))
-				usage = usagebar(((total - avail)/total), 16)
-				printtoarduino(arduino, "Mem utilisation:", usage)
-				time.sleep(refreshint)
+		# 	# mem usage meter
+		# 	endtime = time.time() + functime
+		# 	while endtime > time.time():
+		# 		total = float(runbash('cat /proc/meminfo | grep "MemTotal:" | tr -s " " | cut -d" " -f2'))
+		# 		avail = float(runbash('cat /proc/meminfo | grep "MemAvailable:" | tr -s " " | cut -d" " -f2'))
+		# 		usage = usagebar(((total - avail)/total), 16)
+		# 		printtoarduino(arduino, "Mem utilisation:", usage)
+		# 		time.sleep(refreshint)
 
-			printtoarduino(arduino, "CPU Governor:", runbash("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor").capitalize())
-			time.sleep(3)
+		# 	printtoarduino(arduino, "CPU Governor:", runbash("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor").capitalize())
+		# 	time.sleep(3)
